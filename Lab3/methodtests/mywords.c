eaa1793c5559432061a3d962c30eb26e4afc9808
Richard Garcia <richard.adamo.garcia@gmail.com>
2020-06-21 15:00:43 -0400

    Added test source code in different directory for testing, got the printing working

---
+++ Lab3/methodtests/mywords.c
@@ -0,0 +1,87 @@
+/*
+	example of command line parsing via getopt
+	usage: getopt [-dmp] -f fname [-s sname] name [name ...]
+
+	Paul Krzyzanowski
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int debug = 0;	
+int main(int argc, char **argv)
+{
+	
+	extern char *optarg;
+	extern int optind;
+	int c, err = 0; 
+	int cflag=0, sflag=0, fflag = 0;
+	char *sname = "default_sname", *fname;
+	static char usage[] = "usage: %s [-cs] [-f substring] filename\n";
+	
+//Added this in for printing
+	 
+	char ch;
+	FILE *filePointer;
+	
+
+	while ((c = getopt(argc, argv, "csf:")) != -1)
+		switch (c) {
+		case 'c':
+			debug = 1;
+			cflag = 1;
+			break;
+		case 's':
+			sflag = 1;
+			break;
+		case 'f':
+			fflag = 1;
+			break;
+		case '?':
+			err = 1;
+			break;
+		}
+	if (fflag == 0) {	/* -f was mandatory */
+		fprintf(stderr, "%s: missing -f option\n", argv[0]);
+		fprintf(stderr, usage, argv[0]);
+		//exit(1);
+	} else if ((optind+1) > argc) {	
+		/* need at least one argument (change +1 to +2 for two, etc. as needeed) */
+
+		printf("optind = %d, argc=%d\n", optind, argc);
+		fprintf(stderr, "%s: missing name\n", argv[0]);
+		fprintf(stderr, usage, argv[0]);
+		exit(1);
+	} else if (err) {
+		fprintf(stderr, usage, argv[0]);
+		exit(1);
+	}
+	/* see what we have REMOVE LATER */
+	printf("cflag = %d\n", cflag);
+	printf("sflag = %d\n", sflag);
+	printf("fflag = %d\n", fflag);
+	//DELETE later printf("fname = \"%s\"\n", fname);
+	//DELETE LATERprintf("sname = \"%s\"\n", sname);
+ 	
+	//Test code to print file contents
+	printf("File Name: %s\n", argv[optind]);
+	filePointer = fopen(argv[optind], "r");
+	if (filePointer == NULL)
+   	{
+      		printf("Error while opening the file.\n");
+      		exit(-1);
+   	}
+  	 while((ch = fgetc(filePointer)) != EOF)
+      		printf("%c", ch);
+
+   	fclose(filePointer);
+	
+	if (optind < argc)	/* these are the arguments after the command-line options */
+		for (; optind < argc; optind++)
+			printf("argument: \"%s\"\n", argv[optind]);
+	else {
+		printf("no arguments left to process\n");
+	}
+//	fclose(filePointer);
+	exit(0);
+}
