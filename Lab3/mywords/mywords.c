f970cfa50e2021ca9b58beff3f2d92933bc5ebaa
Richard Garcia <richard.adamo.garcia@gmail.com>
2020-06-21 23:23:51 -0400

    commit without makefile

--- Lab3/mywords/mywords.c
+++ Lab3/mywords/mywords.c
@@ -1,85 +1,79 @@
 /*
-	example of command line parsing via getopt
-	usage: getopt [-dmp] -f fname [-s sname] name [name ...]
-
-	Paul Krzyzanowski
+	This program takes command line arguments and prints texts file contents and text file word count
+	Usage: [-cs] [-f substring ] filename 
+	I affirm that I wrote this program myself without any help form any other people or sources from the internet
+	
+	Richard Garcia
+	ID# 5862119
 */
 
 #include <stdio.h>
 #include <stdlib.h>
-
-int debug = 0;
-
-void printFileContents( char file_name[]){
-	char c;
-	char file_name[100] = file_name;
-	FILE *FilePointer;
-	FilePointer = fopen(file_name, "r");
 	
-	if(FilePointer == NULL){
-		printf("Could not open file %c", file_name);
-		return -1;
-	}
-	else{
-		while((c = fgetc(FilePointer)) != EOF){
-			printf("%c", c);
-		}
-	}
-	fclose(FilePointer);
-	return 0;
-	}
-	
+int debug = 0;	
 int main(int argc, char **argv)
 {
+	
 	extern char *optarg;
 	extern int optind;
 	int c, err = 0; 
-	int mflag=0, pflag=0, fflag = 0, sflag=0;
+	int cflag=0, sflag=0, fflag = 0;
 	char *sname = "default_sname", *fname;
-	static char usage[] = "usage: %s [-cs] [-f substring] filename\n";
+	//Creating the usage statement to print if the user makes an error
+	static char usage[] = "usage: %s [-cs] [-f substring] filename\n";	 
+	char ch;
+	//these file pointers will allow me to print the file to the screen and also get the wordcount
+	FILE *filePointer;
+	FILE *wordCountPointer;
+	int wordcount;
+	
+	
 
 	while ((c = getopt(argc, argv, "csf:")) != -1)
 		switch (c) {
 		case 'c':
 			debug = 1;
+			cflag = 1;
 			break;
 		case 's':
-			mflag = 1;
+			sflag = 1;
 			break;
 		case 'f':
-			pflag = 1;
+			fflag = 1;
 			break;
 		case '?':
 			err = 1;
 			break;
 		}
-	if (fflag == 0) {	/* -f was mandatory */
-		fprintf(stderr, "%s: missing -f option\n", argv[0]);
-		fprintf(stderr, usage, argv[0]);
-		exit(1);
-	} else if ((optind+1) > argc) {	
-		/* need at least one argument (change +1 to +2 for two, etc. as needeed) */
-
-		printf("optind = %d, argc=%d\n", optind, argc);
-		fprintf(stderr, "%s: missing name\n", argv[0]);
-		fprintf(stderr, usage, argv[0]);
-		exit(1);
-	} else if (err) {
-		fprintf(stderr, usage, argv[0]);
-		exit(1);
-	}
-	/* see what we have */
-	printf("debug = %d\n", debug);
-	printf("pflag = %d\n", pflag);
-	printf("mflag = %d\n", mflag);
-	printf("fname = \"%s\"\n", fname);
-	printf("sname = \"%s\"\n", sname);
 	
-	if (optind < argc)	/* these are the arguments after the command-line options */
-		for (; optind < argc; optind++)
-			printf("argument: \"%s\"\n", argv[optind]);
-	else {
-		printf("no arguments left to process\n");
+ 	
+	//This block of code prints the contents of the file 
+
+	filePointer = fopen(argv[optind], "r");
+	printf("File Text is : \n");
+	while((ch = fgetc(filePointer)) != EOF){
+      		printf("%c", ch);
+
 	}
+	fclose(filePointer);
+
+	//This Portion of the code checks if the file exists
+	wordCountPointer = fopen(argv[optind], "r");
+	if (filePointer == NULL)
+   	{
+      		printf("File does not exist\n");
+      		exit(-1);
+   	}
+	
+
+	//This portion of the code performs the word count
+	if(cflag == 1){
+	char currentWord[50];
+	while(fscanf(wordCountPointer, "%s", currentWord ) != EOF){
+		wordcount++;
+	}
+        fclose(wordCountPointer);	
+	printf("Word Count: %d \n", wordcount);
+  	}
 	exit(0);
 }
